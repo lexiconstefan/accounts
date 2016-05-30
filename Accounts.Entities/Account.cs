141c8da62387fd05399bdfe1d980c72d423a9bc0
stefan <lexiconstefanferm@gmail.com>
2016-05-30 08:02:50 +0200

    Add new project

---
+++ Accounts.Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts.Entities
+{
+   public class Account
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string firstname { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string lastname { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string adress { get; set; }
+    }
+}
